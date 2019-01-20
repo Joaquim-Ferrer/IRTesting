f87248c70dd3ca42c899b6f2216cbfe63003aeb4
Maurice Frank <maurice.frank@posteo.de>
2019-01-20 21:25:35 +0100

    morris init

--- ClickModel.py
+++ ClickModel.py
@@ -4,69 +4,67 @@ from YandexParser import parseYandexLog
 import Session
 
 #CHECK IF MAX AT IS OK WITH SIMULATE CLICK AND STUFF AS WE ARE TRAINING FOR 10 RANKINGS AND USING ONLY MAX 6
+class EMX():
+    PROB_MIN = 0.000001
+    def __init__(self, sum=1., count=2.):
+        self.sum = sum
+        self.count = count
+
+    def value(self):
+        return min(self.sum / float(self.count), 1 - self.PROB_MIN)
 
 class BinaryRelevancePBM:
-
     def __init__(self, max_at=6, p_attraction=0.9):
         self.max_at = max_at
-        self.p_examination = np.asarray([random.random() for i in range(max_at)])
+        self.p_examination = np.random.random(max_at)
         self.p_attraction = p_attraction
 
-    def generate_click_probabilities(self, relevances):
-        p_attraction = np.asarray([self.p_attraction if relevance == 1 else 1-self.p_attraction for relevance in relevances])
-        click_probabilities = np.multiply(p_attraction, self.p_examination[:len(relevances)])
-        return click_probabilities
+    def click_probabilities(self, relevances):
+        p_attraction = np.array([self.p_attraction if relevance == 1 else 1-self.p_attraction for relevance in relevances])
+        cDist = p_attraction * self.p_examination[:len(relevances)]
+        return cDist
 
     def simulate_click(self, relevances):
-        click_probabilities = self.generate_click_probabilities(relevances)
-        #Normalize click probabilities
-        click_probabilities = click_probabilities / sum(click_probabilities)
-        chosen_ranking = np.random.choice([i for i in range(relevances)], p = click_probabilities)
+        cDist = self.click_probabilities(relevances)
+        cDist /= cDist.sum()
+        chosen_ranking = np.random.choice(len(relevances), p = cDist)
         return chosen_ranking
 
     #initial_attraction/initial_examination is a function that returns the initial attraction of a query_id, document_id pair
-    def estimate_parameters(self, sessions, n_iterations=100, initial_attraction=lambda: 1, initial_examination=lambda: 0.5):
-        p_examination = [[[0, 0], initial_examination()]  for i in range(10)] #List of [[sum, count], old_examination] for every rank
+    def estimate_parameters(self, sessions, n_iterations=100, at=10):
+        p_examination = [EMX()  for i in range(at)]
         p_attractiveness = {} #Dictionary that maps from (query_id, document_id) to [[sum, count], old_attractiveness]
-        
+
         for i in range(n_iterations):
-            #print([p_examination[i][1] for i in range(len(p_examination))])
             for s in sessions:
                 query_id = s.query_id
                 for rank, document_id in enumerate(s.results):
-                    #Initialize attractiveness
-                    if (query_id, document_id) not in p_attractiveness:
-                        p_attractiveness[(query_id, document_id)] = [[0, 0], initial_attraction()]
-                    #Get values so that it doesn't become a dictionary indexing mess
-                    old_attractiveness = p_attractiveness[(query_id, document_id)][1]
-                    old_examination = p_examination[rank][1]
+                    attractiveness = p_attractiveness.get((query_id, document_id), EMX())
 
-                    p_attractiveness[(query_id, document_id)][0][1] += 1
-                    p_examination[rank][0][1] += 1
+                    oattr = attractiveness.value()
+                    oexam = p_examination[rank].value()
+
+                    attractiveness.count += 1.
+                    p_examination[rank].count += 1.
 
                     if s.clicks_rank[rank]:
-                        p_attractiveness[(query_id, document_id)][0][0] += 1
-                        p_examination[rank][0][0] += 1
+                        attractiveness.sum += 1.
+                        p_examination[rank].sum += 1.
                     else:
-                        p_attractiveness[(query_id, document_id)][0][0] += (1-old_examination   ) * old_attractiveness / (1-old_attractiveness*old_examination)
-                        p_examination[rank][0][0] +=                       (1-old_attractiveness) * old_examination    / (1-old_attractiveness*old_examination)
-            
-            print("EXAMINATION")
-            for p in p_examination:
-                print(p[0][0], p[0][1], p[1])
-            #Update old values and reset count and sum
-            for i in range(len(p_examination)):
-                p_examination[i][1] = p_examination[i][0][0] / p_examination[i][0][1]
-                p_examination[i][0][0] = 0
-                p_examination[i][0][1] = 0
+                        attractiveness.sum      += (1.-oexam) * oattr / (1. - oattr * oexam)
+                        p_examination[rank].sum += (1.-oattr) * oexam / (1. - oattr * oexam)
+                    p_attractiveness[(query_id, document_id)] = attractiveness
 
-            print("ATTRACTION", len(p_attractiveness))
+            print("EXAMINATION")
+            #Update old values and reset count and sum
+            for rank in range(at):
+                print('{:.3e}   {:.3e} {:.3e}'.format(p_examination[rank].sum, p_examination[rank].count, p_examination[rank].value()))
+
+
+            print("ATTRACTION")
             i=0
             for key, value in p_attractiveness.items():
-                if i<1000 and i%100 == 5: 
-                    print(value[0][0], value[0][1], value[1])
+                if i<1000 and i%100 == 5:
+                    print('{:.3e}   {:.3e} {:.3e}'.format(value.sum, value.count, value.value()))
                 i+=1
-                #p_attractiveness[key][1] = value[0][0] / value[0][1]
-                p_attractiveness[key][0][0] = 0
-                p_attractiveness[key][0][1] = 0
             print("\n\n\n")
