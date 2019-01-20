5f11fa1c7536ba3af96a40c610067583b2234c50
Maurice Frank <maurice.frank@posteo.de>
2019-01-20 16:18:15 +0100

    fixed stuff

--- hm.py
+++ hm.py
@@ -1,34 +1,32 @@
+#!/bin/python
 import numpy as np
+from itertools import product
 import random
 
+GIDX = 0
 at = 3
 max_relevance = 1
 #Bin labels include the first and exclude the last.
 bin_labels = [[0.05, 0.1], [0.1,0.2], [0.2,0.3], [0.3,0.4], [0.4,0.5], [0.5,0.6], [0.6,0.7], [0.8,0.9], [0.9,0.95]]
 
-def next_combination(relevances):
-    for i in range(at):
-        if relevances[i] != max_relevance:
-            relevances[i] += 1
-            break
+def relevance_combinations(relevances, _at=0):
+    for rel in range(max_relevance + 1):
+        relevances[_at] = rel
+        if _at + 1 == len(relevances):
+            yield relevances
         else:
-            relevances[i] = 0
-            if i == at-1:
-                return False
-    return True
+            yield from relevance_combinations(relevances, _at+1)
 
-def next_pair_combination(relevances_A, relevances_B):
-    if not next_combination(relevances_A):
-        relevances_A = [0 for i in range(at)]
-        if not next_combination(relevances_B):
-            return False
-    return True
+def generate_all_pairs(size=at):
+    for relevances_A in relevance_combinations(np.zeros(at)):
+        for relevances_B in relevance_combinations(np.zeros(at)):
+            yield (np.array(relevances_A), np.array(relevances_B))
 
-def get_prob_from_relevance(relevance):
-    return (2**relevance - 1) / 2**max_relevance
+def get_prob_from_relevance(relevance, max_relevance=1):
+    return (np.power(2, relevance) - 1) / 2**max_relevance
 
 def calculate_ERR(relevances):
-    thetas = [get_prob_from_relevance(r) for r in relevances]
+    thetas = get_prob_from_relevance(relevances)
     sum_res = 0
     for r in range(at):
         prod = 1
@@ -44,73 +42,76 @@ def find_bin_index(delta_err):
             return i
     return -1
 
-#Recursively generates all possible interleavings according to the documents on the list being the same or not.
-#The result is a list(every possible interleaving) of lists of (relevance, 0/1) where 0 corresponds to a document of A and 1 corresponds to a document of B
-def generate_team_draft_interleavings(relevances_A, relevances_B):
-    len_A = len(relevances_A)
-    len_B = len(relevances_B)
-    res = []
+def generate_same_documents_sets(relevances_A, relevances_B, same_as=([None, None, None], [None, None, None]), _at=0):
+    if _at == len(relevances_A):
+        yield same_as
+        return
+    yield from generate_same_documents_sets(relevances_A, relevances_B, same_as=same_as, _at=_at+1)
+    for idx, relB in enumerate(relevances_B):
+        if relevances_A[_at] == relB and idx not in same_as[0]:
+            same_as[0][_at] = idx
+            same_as[1][idx] = _at
+            yield from generate_same_documents_sets(relevances_A, relevances_B, same_as=same_as, _at=_at+1)
+            same_as[0][_at] = None
+            same_as[1][idx] = None
 
-    if len_A == 0 and len_B == 0:
-        return []
-    
-    relevances = [relevances_A, relevances_B]
-    if len_A > len_B or (len_A == len_B and random.random() > 0.5):
-        chosen_list = 0
-    else:
-        chosen_list = 1
+def prob_softmax(at, tau = 3):
+    ranks = 1/((np.array(range(at)) + 1)**tau)
+    return ranks
 
-    non_chosen_list = (chosen_list + 1) % 2
-    top_result = relevances[chosen_list].pop(0)
-    res_beginning = (top_result, chosen_list)
-    res_end = []
-    #If no document in B matches to that document in A then we don't change the lists
-    res_end.extend(generate_team_draft_interleavings(relevances_A, relevances_B))
-    #Now we take off the elements from the other list that may be the same as the chosen document and generate all the interleavings.
-    for i in range(len(relevances[non_chosen_list])):
-        if relevances[non_chosen_list][i] == top_result:
-            removed_relevance = relevances[non_chosen_list].pop(i)
-            res_end.extend(generate_team_draft_interleavings(relevances_A, relevances_B))
-            relevances[non_chosen_list].insert(i, removed_relevance)
-    
-    #print(res_beginning, res_end)
-    #Now we reconstruct all the possible endings of interleaving with the result got in this function call
-    if len(res_end) == 0:
-        res = [[res_beginning]] 
-    for interleaving in res_end:
-        temp = [res_beginning]
-        temp.extend(interleaving) 
-        res.append(temp)    
+def generate_interleaving(relevances_A, relevances_B, initDist, chooseFunction, k=50):
+    relevances_AB = (relevances_A, relevances_B)
+    for same_as, _ in product(generate_same_documents_sets(relevances_A, relevances_B), range(k)):
+        probDistr = initDist.copy()
+        relevances, attribution = [], []
+        while np.sum(probDistr) > 0:
+            R = random.randint(0,1)
+            if probDistr[R].sum() == 0:
+                continue
+            Didx = chooseFunction(probDistr, R)
+            probDistr[R][Didx] = 0
+            if same_as[R][Didx]:
+                probDistr[(R+1)%2][same_as[R][Didx]] = 0
+            relevances.append(relevances_AB[R][Didx])
+            attribution.append(R)
+        yield relevances, attribution
 
-    relevances[chosen_list].insert(0, top_result)
-    return res
+def generate_team_draft_interleavings(relevances_A, relevances_B, k=50):
+    initDist = np.ones((2,at),bool)
+    chooseFunction = lambda probDistr, R: probDistr[R].argmax()
+    yield from generate_interleaving(relevances_A, relevances_B, initDist, chooseFunction)
+
+def generate_probabilistic_interleavings(relevances_A, relevances_B):
+    initDist = np.array([prob_softmax(at), prob_softmax(at)])
+    chooseFunction = lambda probDistr, R: np.random.choice(at, p=probDistr[R]/probDistr[R].sum())
+    yield from generate_interleaving(relevances_A, relevances_B, initDist, chooseFunction)
+
+def generate_marginalized_probabilistic_interleavings(relevances_A, relevances_B):
+    pass
+
+def generate_DERR_bins():
+    filled_bins = [[] for i in range(len(bin_labels))] # list of [(relevance_E, relevance_P), ...]. Each list corresponds to the bin with limits in bin_labels
+    for relevances_E, relevances_P in generate_all_pairs():
+        err_E = calculate_ERR(relevances_E)
+        err_P = calculate_ERR(relevances_P)
+        delta_err = err_E - err_P
+        bIdx = find_bin_index(delta_err)
+        if bIdx != -1:
+            filled_bins[bIdx].append((relevances_E, relevances_P))
+    return filled_bins
+
+def main():
+    bins = generate_DERR_bins()
+    I = 0
+    for bIdx, _bin in enumerate(bins):
+        print(bin_labels[bIdx])
+        for pair in _bin:
+            for relevances, attribution in generate_team_draft_interleavings(pair[0], pair[1]):
+                I += 1
+                pass
+                # print(relevances)
+    print(I)
 
 
-#Generate pairs per bin
-bins = [[] for i in range(len(bin_labels))] # list of [(relevance_E, relevance_P), ...]. Each list corresponds to the bin with limits in bin_labels
-relevances_E = [0 for i in range(at)]
-relevances_P = [0 for i in range(at)]
-while True:
-    err_E = calculate_ERR(relevances_E)
-    err_P = calculate_ERR(relevances_P)
-    #print(relevances_E, err_E, relevances_P, err_P)
-    delta_err = err_E - err_P
-    bin_i = find_bin_index(delta_err)
-    #print(delta_err, bin_labels[bin_i] if bin_i != -1 else "nada")
-    if bin_i != -1:
-        fat_object = (relevances_E[:], relevances_P[:]) #Shallow copies as we modify the list when generating the next element
-        bins[bin_i].append(fat_object)
-    #Generate next pair
-    if not next_pair_combination(relevances_E, relevances_P):
-        break
-
-for b_i, bini in enumerate(bins):
-    print(bin_labels[b_i])
-    for pair in bini:
-        print(pair)
-        interleavings = generate_team_draft_interleavings(pair[0], pair[1])
-        for interleaving in interleavings:
-            print(interleaving)
-            pass
-        print("\n\n\n")
-            
+if __name__ == "__main__":
+    main()
